a9b178e967d80d0f0eedffb2794bd46ab724ba07
Jessica Chen <Jessica10chen@gmail.com>
2022-10-16 19:17:40 -0400

    code for buttons on main page to go to other pages

--- app/src/main/java/com/example/mealer/MainActivity.java
+++ app/src/main/java/com/example/mealer/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btnSignInClick(View view){
+        startActivity(new Intent(MainActivity.this, SignIn.class));
+    }
+
+    public void btnClientSignUpClick(View view){
+        startActivity(new Intent(MainActivity.this, ClientSignUp.class));
+    }
+
+    public void btnCookSignUpClick(View view){
+        startActivity(new Intent(MainActivity.this, CookSignUp.class));
+    }
+
 }
