818f596dcf515487568bd600e2fdd16c79552457
Tyler <blinsontyler@gmail.com>
2022-10-17 20:51:12 -0400

    Added User, Client, Cook, Admin and Meal class

--- app/src/main/java/com/example/mealer/SignIn.java
+++ app/src/main/java/com/example/mealer/SignIn.java
@@ -2,7 +2,9 @@ package com.example.mealer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignIn extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void backbtnSignIn(View view){
+        startActivity(new Intent(SignIn.this, MainActivity.class));
+    }
+    public void btn_SignIn(View view){
+        startActivity(new Intent(SignIn.this, Home.class));
     }
 }
