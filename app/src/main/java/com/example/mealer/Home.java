88fdb8426d85788085bf4c7dc22481c9a784060b
Jessica Chen <Jessica10chen@gmail.com>
2022-10-21 21:27:04 -0400

    debug

--- app/src/main/java/com/example/mealer/Home.java
+++ app/src/main/java/com/example/mealer/Home.java
@@ -13,14 +13,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
 public class Home extends AppCompatActivity {
 
     private DatabaseReference dbRef;
+    private FirebaseAuth mAuth;
+    DatabaseReference databaseUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,25 @@ public class Home extends AppCompatActivity {
         welcomeMessage();
 
          */
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databaseUsers = database.getReference("Users");
+        String id = mAuth.getCurrentUser().getUid();
+        DatabaseReference username = databaseUsers.child(id).child("type");
+
+        username.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String type = dataSnapshot.getValue().toString();
+                TextView greetingText= (TextView) findViewById(R.id.greetingtxt);
+//              greetingText.setText("You are logged in as "+ type);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
     public void btnLogOutClick(View view){
         startActivity(new Intent(Home.this, MainActivity.class));
@@ -42,20 +65,22 @@ public class Home extends AppCompatActivity {
     }
 
 
-    public void welcomeMessage(View view) {
-        dbRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (task.isSuccessful()) {
-                    User_Class user = (User_Class) task.getResult().getValue();
-                    String type = user.get_type();
-                    TextView greetingText= (TextView) findViewById(R.id.greetingtxt);
-                    greetingText.setText("You are logged in as "+ type);
 
-                }
-            }
-        });
-    }
+
+//    public void welcomeMessage(View view) {
+//        dbRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DataSnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    User_Class user = (User_Class) task.getResult().getValue();
+//                    String type = user.get_type();
+//                    TextView greetingText= (TextView) findViewById(R.id.greetingtxt);
+//                    greetingText.setText("You are logged in as "+ type);
+//
+//                }
+//            }
+//        });
+//    }
 
 
 
