cf0d6f5403e22f59beaf6783f08651971f4a8dab
tlati083@uottawa.ca <tlati083@uottawa.ca>
2022-11-06 11:49:54 -0500

    fixed sign in page redirection

--- app/src/main/java/com/example/mealer/SignIn.java
+++ app/src/main/java/com/example/mealer/SignIn.java
@@ -3,8 +3,10 @@ package com.example.mealer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,20 +17,31 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignIn extends AppCompatActivity implements View.OnClickListener {
     private EditText edittxt_email, edittxt_password;
     private Button btn_SignIn;
     private FirebaseAuth mAuth;
+    private DatabaseReference databaseUsers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
         edittxt_email = findViewById(R.id.Email_Field3);
         edittxt_password = findViewById(R.id.Password_Field3);
+
         btn_SignIn = (Button) findViewById(R.id.SignIn_btn);
         btn_SignIn.setOnClickListener(this);
+
         mAuth = FirebaseAuth.getInstance();
+        databaseUsers = (DatabaseReference) FirebaseDatabase.getInstance().getReference("Users");
     }
 
     public void backbtnSignIn(View view){ //goes back to main activity
@@ -62,7 +75,33 @@ public class SignIn extends AppCompatActivity implements View.OnClickListener {
             public void onComplete(@NonNull Task<AuthResult> task) {//login function
                 if(task.isSuccessful()){
                     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                    startActivity(new Intent(SignIn.this, HomeClient.class));
+
+                    databaseUsers.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            User_Class currUser = (User_Class) snapshot.getValue(User_Class.class);
+                            String type = currUser.get_type();
+
+                             Intent homeIntent = new Intent();
+
+                            if (type.equals("ADMIN")) {
+                                homeIntent = new Intent(SignIn.this,HomeAdmin.class);
+                            } else if (type.equals("CLIENT")) {
+                                homeIntent = new Intent(SignIn.this, HomeClient.class);
+                            } else if (type.equals("COOK")) {
+                                homeIntent = new Intent(SignIn.this, HomeCook.class);
+                            }
+                            startActivity(homeIntent);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Log.d("TAG",error.getMessage());
+                        }
+                    });
+
+
+                    //startActivity(new Intent(SignIn.this, HomeClient.class));
                 }
                 else{
                     Toast.makeText(SignIn.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
--- app/src/main/java/com/example/mealer/User_Class.java
+++ app/src/main/java/com/example/mealer/User_Class.java
@@ -1,60 +1,61 @@
 package com.example.mealer;
 
-public abstract class User_Class {
-    private String name;
-    private String last_name;
-    private String email;
-    private String password;
+public class User_Class {
+    private String _name;
+    private String _last_name;
+    private String _email;
+    private String _password;
 
-    private String address;
-    private String type;
+    private String _address;
+    private String _type;
 
+    public User_Class () {}
 
     public User_Class(String name , String last_name, String email , String password ,  String address , String type ){
 
-        this.name = name;
-        this.last_name = last_name;
-        this.email = email;
-        this.password = password;
-        this.address = address;
-        this.type = type;
+        this._name = name;
+        this._last_name = last_name;
+        this._email = email;
+        this._password = password;
+        this._address = address;
+        this._type = type;
 
     }
     public String get_name(){
-        return name;
+        return _name;
     }
     public String get_last_name(){
-        return last_name;
+        return _last_name;
     }
     public String get_email(){
-        return email;
+        return _email;
     }
     public String get_password(){
-        return password;
+        return _password;
     }
     public String get_address(){
-        return address;
+        return _address;
     }
     public String get_type(){
-        return type;
+        return _type;
     }
     public void set_name(String name){
-        this.name = name;
+        this._name = name;
     }
     public void set_last_name(String last_name){
-        this.last_name = last_name;
+        this._last_name = last_name;
     }
     public void set_email(String email){
-        this.email = email;
+        this._email = email;
     }
     public void set_password(String password){
-        this.password = password;
+        this._password = password;
     }
     public void set_address(String address){
-        this.address = address;
+        this._address = address;
     }
     public void set_type(String type){
-        this.type = type;
+        this._type = type;
     }
 
 
