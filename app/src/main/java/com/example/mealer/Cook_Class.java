06a39d19c8279cb657b45819df2095dad8f609c4
Jessica Chen <Jessica10chen@gmail.com>
2022-11-06 19:39:15 -0500

    add suspended and suspensiondate variables to cook class

--- app/src/main/java/com/example/mealer/Cook_Class.java
+++ app/src/main/java/com/example/mealer/Cook_Class.java
@@ -8,6 +8,8 @@ public class Cook_Class extends User_Class {
     private String country;
     private String unitNum;
     private String void_check_URL;
+    private boolean suspended;
+    private String suspensionDate;
 
     public Cook_Class(String first_name , String last_name, String email , String password , String address , String unitNum , String postalCode , String country ,String type , String void_check_URL , String description){
         super(first_name , last_name, email , password ,  address , type);
@@ -16,6 +18,8 @@ public class Cook_Class extends User_Class {
         set_unit_num(unitNum);
         set_void_check(void_check_URL);
         set_description(description);
+        set_suspended(false);
+        set_suspensionDate("");
     }
     public String get_void_check_URL(){
         return void_check_URL;
@@ -26,15 +30,17 @@ public class Cook_Class extends User_Class {
     public String get_postal_code() { return postalCode; }
     public String get_country() { return country; }
     public String get_unit_num() { return unitNum; }
+    public boolean get_suspended(){ return suspended; }
+    public String get_SuspensionDate(){ return suspensionDate}
     public void set_postal_code(String postalCode) { this.postalCode = postalCode; }
     public void set_country(String country) { this.country = country; }
     public void set_unit_num(String unitNum) { this.unitNum = unitNum; }
     public void set_void_check(String void_check){
         this.void_check_URL = void_check;
     }
-    public void set_description(String description){
-        this.description = description;
-    }
+    public void set_description(String description){this.description = description;}
+    public void set_suspended(boolean suspended){this.suspended = suspended;}
+    public void set_suspensionDate(String suspensionDate){this.suspensionDate = suspensionDate;}
     public void create_meal(String name, String description, String price, String Meal_type, String Cuisine_type, String[] ingredients, String[] allergens){
         Meal_Class meal = new Meal_Class(name, description, price, Meal_type, Cuisine_type, ingredients, allergens);
 
