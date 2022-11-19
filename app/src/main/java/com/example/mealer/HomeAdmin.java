735bc2b6b6894891d3b351f15b7a3a1d51398aec
tlati083@uottawa.ca <tlati083@uottawa.ca>
2022-11-18 21:47:14 -0500

    fixed logout button on Admin Home page

--- app/src/main/java/com/example/mealer/HomeAdmin.java
+++ app/src/main/java/com/example/mealer/HomeAdmin.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -157,6 +159,11 @@ public class HomeAdmin extends AppCompatActivity {
             }
         });
     }
+    public void btnLogOutClick(View view){
+        startActivity(new Intent(HomeAdmin.this, MainActivity.class));
 
+        FirebaseAuth.getInstance().signOut();
+        //finish();
+    }
 
 }
