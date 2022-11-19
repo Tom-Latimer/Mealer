1ac1225c7d1794e516fb608ecd3ce2d8a06fd77c
Owen Halvorson <ohalv049@uottawa.ca>
2022-11-18 19:14:05 -0500

    Changed to testCases

---
+++ app/src/test/java/com/example/mealer/MenuTest.java
@@ -0,0 +1,15 @@
+package com.example.mealer;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class MenuTest {
+
+    @Test
+    public void testOfferToggled(String id){
+
+
+    }
+
+}
