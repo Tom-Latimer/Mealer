1c56f0b4f4ac5c850f222b5dfbf5e715d96cfb52
Jessica Chen <Jessica10chen@gmail.com>
2022-12-07 20:27:36 -0500

    edit submit complaint

--- app/src/main/java/com/example/mealer/ClientPurchaseRequestList.java
+++ app/src/main/java/com/example/mealer/ClientPurchaseRequestList.java
@@ -5,14 +5,32 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.nio.file.attribute.FileTime;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class ClientPurchaseRequestList extends ArrayAdapter<PurchaseRequest> {
 
@@ -66,14 +84,59 @@ public class ClientPurchaseRequestList extends ArrayAdapter<PurchaseRequest> {
             @Override
             public void onClick(View v) {
                 // startActivity(new Intent(MainActivity.this, MyOtherActivity.class));
+                View parentRow = (View) v.getParent();
+                ListView listView = (ListView) parentRow.getParent();
+                final int position = listView.getPositionForView(parentRow);
+
+                PurchaseRequest purchaseRequest = purchaseRequests.get(position);
+                showSubmitComplaintDialog(purchaseRequest);
             }
         });
 
         return listViewItem;
 
-
     }
 
+    private void showSubmitComplaintDialog(PurchaseRequest purchaseRequest) {
+
+        // final String complaintId = purchaseRequest.getMeal();
+        String meal = String.valueOf(purchaseRequest.getMeal());
+        String cook = String.valueOf(purchaseRequest.getCookName());
+
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext());
+        FileTime getLayoutInflater;
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        final View dialogView = inflater.inflate(R.layout.activity_submit_complaint, null);
+        dialogBuilder.setView(dialogView);
+
+        final TextView txtMeal  = (TextView) dialogView.findViewById(R.id.textViewMealItem);
+        final TextView txtCook  = (TextView) dialogView.findViewById(R.id.textViewCookItem);
+        final EditText txtComplaintMessage = (EditText) dialogView.findViewById(R.id.editTextComplaintMessage);
+        final Button btnSubmit = (Button) dialogView.findViewById(R.id.btnSubmit);
+
+        txtMeal.setText(meal);
+        txtCook.setText(cook);
+
+        final AlertDialog builder = dialogBuilder.create();
+        builder.show();
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!txtComplaintMessage.equals("")) {
+                    Date currentDate = new Date();
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                    String strCurrentDate = formatter.format(currentDate);
+
+                    Complaint Complaint = new Complaint();
+                    Complaint.add_complaint(new Complaint(id, purchaseRequest.getClientName(), txtComplaintMessage.getText().toString(), strCurrentDate, purchaseRequest.getCookName()));
+                }
+
+                builder.dismiss();
+            }
+        });
+    }
 
 
 }
--- app/src/main/java/com/example/mealer/Complaint.java
+++ app/src/main/java/com/example/mealer/Complaint.java
@@ -1,11 +1,18 @@
 package com.example.mealer;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Complaint {
 
     String _complaintDate;
     String _complaintMessage;
     String _complaintRecipient;
     String _complaintId;
+//    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//    String uid = user.getUid();
 
     public Complaint(){
 
@@ -38,4 +45,16 @@ public class Complaint {
 
     public void setComplaintRecipient(String recipient) {_complaintRecipient = recipient; }
 
-}
+    public void add_complaint(Complaint complaint){
+        //check if meal is null
+
+        // DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Complaint").child(uid);
+
+//        String id = ref.push().getKey();
+//        complaint.setComplaintId(id);
+//        complaint.setComplaintMessage();
+//        complaint.set_cookID(uid);
+//        ref.child(id).setValue(meal);
+
+
+    }
